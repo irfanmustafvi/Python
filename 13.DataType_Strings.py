229f2ac226edc402a233463c2fa881b0fb5b5b75
irfanmustafvi <irfam.4831@gmail.com>
2024-08-02 17:53:53 +0300

    v1 commit

---
+++ 13.DataType_Strings.py
@@ -0,0 +1,46 @@
+# String multi line function
+# greeting = "Hello: "
+# question = " How are you?"
+# message = greeting + question
+# print(message)
+
+# greeting = "Hello:"
+# question = "How are you?"
+# message = greeting + " " + question
+# print(message)
+
+# Repeating variable
+# laugh = "ha"
+# laugh *= 3
+# print(laugh)
+
+# multi-line string variable  ## """"ad """ three quotes are used to write multi line code i string
+# multi_line = """"This is multi line transcript to
+# evaluate the script in string"""
+# print(multi_line)
+
+# built in length function
+# password = "password123456"
+# pw_length = len(password) # total item in password are sum up
+# print(pw_length)
+
+# Indexing # here number of item {letter} is printed as mention as my_string[8]
+# my_string = "Hello world"
+# print(my_string[0])
+
+# Slicing # here we put {colon} is printed as complete string
+# my_string = "Hello world"
+# print(my_string[:])
+# my_string = "Hello world"  # Ranges left side of colon and right side of colon
+# print(my_string[:5])
+# my_string = "Hello world!"  # Ranges left side of colon and right side of colon
+# print(my_string[-6:])
+# my_string = "Hello world!"
+# print(my_string[1:10:1])
+
+# Docs Python.org @ String Methods
+# my_string = "hello world!"  # When we put dot in print function > different types of functions are listed.
+# print("this is long string".capitalize())  # this will change first letter capital
+
+my_string = "Hello World!"  # casefold function
+print(my_string.casefold())  # this will change first letter to lowercase
