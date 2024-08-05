fdf9ac99a68fbeb30028386867f9277b524defbc
irfanmustafvi <irfam.4831@gmail.com>
2024-08-05 18:29:12 +0300

    Updated commit

--- 13.DataType_Strings.py
+++ 13.DataType_Strings.py
@@ -58,10 +58,10 @@
 # print("your" in my_string)
 
 # Section 27
-space_string = "          Too many space more than ten          "
-dash_string = "--- too many dashes---"
-symbol_string = "<@>?#% ****** !~ Too many symbols_^~!@#$%^&"
-quot = "It is not a bug, it is a feature"
+# space_string = "          Too many space more than ten          "
+# dash_string = "--- too many dashes---"
+# symbol_string = "<@>?#% ****** !~ Too many symbols_^~!@#$%^&"
+# quot = "It is not a bug, it is a feature"
 # print(space_string.strip())  # This string move left side
 # print(dash_string.lstrip("-")) #left side dashes are removed
 # print(dash_string.rstrip("-"))
@@ -73,7 +73,56 @@ quot = "It is not a bug, it is a feature"
 # print(quot.split())  # ['It', 'is', 'not', 'a', 'bug,', 'it', 'is', 'a', 'feature']
 # print(quot.split("is"))  # this will remove "is" & replace with commas
 # print(dash_string.strip("-").upper().split()) # all convert to uppercase.
-my_list = dash_string.strip("-").upper().split()
+# my_list = dash_string.strip("-").upper().split()
 # print(" ".join(my_list))  # nthis way joined the words
 # print("---".join(my_list))
-print(" : ".join(dash_string))
+# print(" : ".join(dash_string))
+
+# Section 28 Escape Character in String
+# my_string = "Hello,wo\\rld!" # back slash function
+# print(my_string)
+# my_string = "Hello,'world!'"  # Double quotes
+# my_string = "hello, 'world'"
+# my_string = "Hell0 \nworld, this is line \nseparation"  #  New line character \n
+# my_string = "Hell0 \tworld, this is line \tseparation"  # \t for tab space in line
+# my_string = "Hello \rworld"  # \r line starts from here, before words are deleted
+# my_string = "Hell0 world, \bthis is \tseparation"  # \b remove backspace character
+# my_string = "Hell0 \x0aworld, "  # This also separate line
+# my_string = "Hell0 world, \u263a"  # this will add smile face
+# my_string = "Hell0 world, \N{snowman}"
+# print(my_string)
+
+# Section 29 Formating of string CHECK From Py Docs
+# name = "zia"
+# age = 28
+# job = "developer"
+# person_string = "Manager name is {}, {} is age {}; and job {}"
+# person_string = (
+#     "Manager name is {2}, {2} is age {0}; and job is {1}"  # Indexing the variable
+# )
+# person_string = "Manager name is {_name}, {_name} is age {0}; and job {1}"  # Key variable(name changed) pair
+# # print(person_string.format(age, job, name))  # Format function is used for
+# # print(person_string.format(age, job, _name="Dasmon"))
+# person_string = f"Manager name is {name}, {name} is age {age}; and job {job}"  # Best method for format
+# # print(person_string)
+# pi = 3.141592
+# pi_str = f"the value of pi approximately {pi:0.4f}"  # 0.4f present digit after decimal
+# pi_str = f"the value of pi approximately {pi:>20}"  # :> increases space
+# print(pi_str)
+
+# Section 30- Mad Lib Challenge
+"""
+In this challenge, going to create a mad lib generator! You should 
+start by printing a welcome message to the console. Then, get user input for a 
+noun, verb, adjective, and adverb and store the values in variables. After 
+that, format a string to tell a short story using the user's inputs and store 
+it in a variable. Finally, output the story to the console.
+"""
+
+# Print a welcome message to the console
+
+# Get inputs for a noun, verb, adjective, and adverb and store them in variables
+
+# Format a string be a story with the 4 user inputs and store in in a variable
+
+# Print the formatted story string to the console
