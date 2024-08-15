de42e803eeb76a6d92716df54dd5999458acda41
irfanmustafvi <irfam.4831@gmail.com>
2024-08-15 22:51:50 +0300

    Updated commit

--- 14.DataType_Lists.py
+++ 14.DataType_Lists.py
@@ -33,3 +33,17 @@
 ## Removing Elements
 # my_list.clear()  # result empty list []
 # print(my_list)
+## 6/35 List change order of element
+# animals = ["dog", "cat", "cow", "horse", "duck", "goat", "chicken"]
+# print(animals)
+# print(animals.sort())  # output none
+# print(animals) # result is sorted list
+# print(animals.sort(key=len)) # output is sorted from short to long spelling
+# print(animals.sort(key=len, reverse=True))  # this will reverse order from long-short
+# print(animals.reverse())
+# result = sorted(animals)
+# result = sorted(animals, key=len)  # sorted by length short - long
+# result = sorted(animals, key=len, reverse=False)  # sortrd by short -long with false
+# result = sorted(animals, key=len, reverse=True) # sorted by long-short with true
+# result = sorted("Hello , World!")  # This is not perfect function to sort
+# print(result)
