fb77ac770d8906f4cc12dce49148769360162f1c
irfanmustafvi <irfam.4831@gmail.com>
2024-08-19 18:08:22 +0300

    Set Commit

--- 18.DataType_Sets.py
+++ 18.DataType_Sets.py
@@ -3,19 +3,19 @@ my_set = {1, 2, 3, 2, 3}
 print(my_set)  # output {1, 2, 3} by ignore (2, 3)
 my_set = {}
 print(my_set)  # output {}
-my_set = set{} # with curly braces { } give error
+# my_set = set{} # with curly braces { } give error
 my_set = set()  # output set()
 my_set = set([1, 2, 3])  # output curly braces {1,2,3}
 
 ## 8/45 Adding/Removing Elements
 my_set = {1, 2, 3}
-my_set.add([4]) # Output type error so number must be part of set
+my_set.add([4])  # Output type error so number must be part of set
 my_set.remove(4)  # output keyerror:4
-my_set.remove(1) # output {2, 3}
+my_set.remove(1)  # output {2, 3}
 my_set.discard(4)  # output will be remain as {1, 2, 3} [4 is not part of set]
 my_set.discard(2)  # output is {1, 3}
 my_set.clear()  # output empty set()
-del my_set # output is naming error
+del my_set  # output is naming error
 my_set = {3, 2, 1}  # output is {1, 2, 3}
 my_set = set("world")
 print(my_set.pop(), my_set)  # output r {'w', 'd', 'l', 'o'}
@@ -31,14 +31,14 @@ my_set1 = {1, 2, 3, 4}
 my_set2 = {3, 4, 5, 6}
 my_set3 = {1, 2, 3}
 my_set4 = {5, 6, 7, 8}
-result = my_set1.difference(my_set2) # output {1, 2}
-result = my_set1 - my_set2 # output same with subtraction operator{1, 2}
-result = my_set1.intersection(my_set2) # output is {3, 4}
-result = my_set1 & my_set2 # output same with operator & = {3, 4}
+result = my_set1.difference(my_set2)  # output {1, 2}
+result = my_set1 - my_set2  # output same with subtraction operator{1, 2}
+result = my_set1.intersection(my_set2)  # output is {3, 4}
+result = my_set1 & my_set2  # output same with operator & = {3, 4}
 result = my_set1.union(my_set2)  # output union of sets{1,2,3,4,5,6}
-result = my_set1 | my_set2 # output same with pipe as in union
+result = my_set1 | my_set2  # output same with pipe as in union
 result = my_set1.symmetric_difference(my_set2)  # output only difference of both sets
-result = my_set1 ^ my_set2 # same output with symbol ^ (alt+94) {1,2,5,6}
+result = my_set1 ^ my_set2  # same output with symbol ^ (alt+94) {1,2,5,6}
 my_set1.difference_update(my_set2)  # output {1, 2}= set1
 my_set1 -= my_set2  # with operators same eoutput {1,2}
 print(my_set1)
@@ -50,4 +50,3 @@ result = my_set3.issuperset(my_set1)  # False if elements are not in superset
 result = my_set3.issubset(my_set1)  # True set3 has less element of set1
 result = my_set3.issubset(my_set2)  # False set3 has less element of set2
 print(result)
-
