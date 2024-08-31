a4b052949fab77b85255c71ae6cd938092d0980e
irfanmustafvi <irfam.4831@gmail.com>
2024-08-31 20:53:03 +0300

    Change Folder Commit

--- 01.Input_output.py
+++
@@ -1,9 +0,0 @@
-# Input & Output 
-print("hi, this is my first code in python 21.07.24")
-#use name variables
-name =input("Enter your name")
-print(name)
-n1 =input("Enter number=")
-n2 =input("Enter number=")
-print(n1 + n2)
-# How to convert string number in integer form (in next classes)?
