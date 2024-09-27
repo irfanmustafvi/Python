56e273273565fd4014fcd08b7412786bba82085c
irfanmustafvi <irfam.4831@gmail.com>
2024-09-27 20:28:19 +0300

    Class & OOP commit

--- 04.Challenge_input.py
+++ 04.Challenge_input.py
@@ -3,9 +3,9 @@ first name and one for last name. Then, print a greeting that outputs
 "Hello, [your first and last name]"
 """
 
-# Create a variable for your first name
+# Create a variable for first name
 first_name = input("Enter first name ")
-# Create a variable for your last name
+# Create a variable for last name
 last_name = input("Enter last name ")
 # Print a greeting to the console
 print("Hello,", first_name, last_name)
--- 27.Class_OOP.py
+++ 27.Class_OOP.py
@@ -163,46 +163,182 @@
 # print(f"add: {add}\nmultiply: {multiply}\nsquare: {square}")
 ###################################################################
 ## SECTION 15/91 Class Magic Method
-class Car:
-    def __init__(self, color, make, model, year):
-        self.color = color
-        self.make = make
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, color, make, model, year):
+#         self.color = color
+#         self.make = make
+#         self.model = model
+#         self.year = year
 
-    def __str__(self) -> str:
-        return f"A {self.color} {self.make} {self.model} made in {self.year}"
+#     def __str__(self) -> str:
+#         return f"A {self.color} {self.make} {self.model} made in {self.year}"
 
 
-car1 = Car("Black", "Chevy", "Silverado", 2023)
-car2 = Car("Red", "Ford", "Explorer", 2020)
-print(car1)  # Output A Black Chevy Silverado made in 2023
-print(car2)  # Output A Red Ford Explorer made in 2020
+# car1 = Car("Black", "Chevy", "Silverado", 2023)
+# car2 = Car("Red", "Ford", "Explorer", 2020)
+# print(car1)  # Output A Black Chevy Silverado made in 2023
+# print(car2)  # Output A Red Ford Explorer made in 2020
 
 
 ################################################################################
-class Car:
-    def __init__(self, color, make, model, year):
-        self.color = color
-        self.make = make
-        self.model = model
-        self.year = year
+# class Car:
+#     def __init__(self, color, make, model, year):
+#         self.color = color
+#         self.make = make
+#         self.model = model
+#         self.year = year
 
-    def __str__(self) -> str:
-        return f"A {self.color} {self.make} {self.model} made in {self.year}"
+#     def __str__(self) -> str:
+#         return f"A {self.color} {self.make} {self.model} made in {self.year}"
 
-    def __eq__(self, other) -> bool:
-        if isinstance(other, Car):
-            return (
-                self.make == other.make
-                and self.model == other.model
-                and self.year == other.year
-            )
-        return False
+#     def __eq__(self, other) -> bool:
+#         if isinstance(other, Car):
+#             return (
+#                 self.make == other.make
+#                 and self.model == other.model
+#                 and self.year == other.year
+#             )
+#         return False
 
 
-car1 = Car("Black", "Chevy", "Silverado", 2023)
-car2 = Car("Red", "Ford", "Explorer", 2020)
-car3 = Car("Blue", "Chevy", "Silverado", 2023)
-print(car1 == car2)
-print(car1 == car3)
+# car1 = Car("Black", "Chevy", "Silverado", 2023)
+# car2 = Car("Red", "Ford", "Explorer", 2020)
+# car3 = Car("Blue", "Chevy", "Silverado", 2023)
+# print(car1 == car2)
+# print(car1 == car3)
+#################################################################################################
+## Section 15/92 Inheritance class
+# class Desert:
+#     def __init__(self, name, flavor):
+#         self.name = name
+#         self.flavor = flavor
+
+#     def cook(self):  # inheritance derived from class
+#         print(f"You bake the {self.flavor} {self.name}!")
+
+
+# class Cookie(Desert):
+#     def dunk(self):
+#         print(f"You dunk the {self.name} in milk!")
+
+
+# pie = Desert("pie", "apple")
+# cookie = Cookie("cookie", "chocolate chip")
+
+
+# pie.cook()  # output You bake the apple pie!
+# cookie.cook()  # Output You bake the chocolate chip cookie!
+# # pie.dunk() # Output is error Attribute Error
+# cookie.dunk() # Output You dunk the cookie in milk!
+##############################################################################
+# class Desert:
+#     def __init__(self, name, flavor):
+#         self.name = name
+#         self.flavor = flavor
+
+#     def cook(self):  # inheritance derived from class
+#         print(f"You bake the {self.flavor} {self.name}!")
+
+
+# class Cookie(Desert):
+#     def __init__(self, name, flavor, price):
+#         # Desert.__init__(self, name, flavor)
+#         super().__init__(name, flavor)  # output 4.99
+#         self.price = price
+
+#     def dunk(self):
+#         print(f"you dunk the {self.name} in milk!")
+
+#     def bake_and_sell(self):
+#         super().cook()
+#         print(f"You sell the {self.flavor} {self.name} for ${self.price}!")
+
+
+# tart = Cookie("cookie", "cherry tart", 6.99)  # Output You bake the cherry tart cookie!
+# cookie = Cookie(
+#     "cookie", "chocolate chip", 4.99
+# )  # output You bake the chocolate chip cookie!
+# tart.bake_and_sell()  # output You sell the cherry tart cookie for $6.99!
+# cookie.bake_and_sell()  # output You sell the chocolate chip cookie for $4.99!
+############################################################
+##Practice all above #####################################################
+## Section 16/93 Error Handling
+# def positive_value(x):
+#     if x < 0:
+#         raise ValueError
+#     else:
+#         print(f"The value {x} is positive")
+# try:
+#     positive_value(-2)
+# except ValueError as e:
+#     print("error:", e) # Output Error:
+#####################################################################
+# print("error:", e)  # Output Error:x should be a positive value!
+####################################################################################
+## Custom exceptions
+# class InvalidEmailError(Exception):
+#     def __init__(self, message):
+#         super().__init__(f"Invalid email address: {message}")
+#         self.message = message
+
+
+# def validate_email(email):
+#     if "@" not in email or "." not in email:
+#         raise InvalidEmailError(email)
+
+
+# try:
+#     email = input("Enter an email address: ")
+#     validate_email(email)
+#     print("Email is valid:", email)
+# except InvalidEmailError as e:
+#     print("Error:", e)
+###############################################################################
+## Section 15/94 Classes Polymorphism
+## Main class in Object Oriented Program
+class Monster:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def attack(self):
+        print(f"{self.name} attacks!")
+
+
+class Dragon(Monster):
+    def __init_(self, name, health, element):
+        super().__init__(name, health)
+        self.element = element
+
+    def attack(self):
+        print(f"{self.name} breaths {self.element}!")
+
+
+class Goblin(Monster):
+    def __init__(self, name, health, weapon):
+        super().__init__(name, health)
+        self.weapon = weapon
+
+    def attack(self):
+        print(f"{self.name} swings a {self.weapon}!")
+
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        print(f"{self.name} attacks the string!")
+
+
+def attack(monster):
+    monster.attack()
+
+
+rat = Monster("rat", 10)
+goblin = Goblin("goblin", 100, "sword")
+cat = Cat("Mittens")
+
+attack(rat)  # Output rat attacks!
+attack(goblin)  # Output goblin swings a sword!
+attack(cat)  # Output Mittens attacks the string!
